838853bcf618898c1f54871c6c7dafb5a50cdadc
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-05-23 20:11:00 +0300

    v2

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import * as css from "file.css";
 
 var lightbox = new SimpleLightbox('.gallery a',  {
     captions: true, // Увімкнути підписи
